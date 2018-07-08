bb50fb7398b4db08e633ebcc47a46fcfab1630d5
waegaein <waegaein@gmail.com>
2018-07-08 20:13:42 +0900

    Introduce class

--- QualificationRound/solution.py
+++ QualificationRound/solution.py
@@ -1,25 +1,29 @@
 import sys
 import filehandler as fh
 
-def p1():
-    print('p1')
-    inputReader = fh.InputReader('input')
-    strings = inputReader.readFile('tourist_example_input.txt')
-    print(strings)
+class Solution:
+    def __init__(self):
+        self.inputReader = fh.InputReader('input')
 
-def p2():
-    print('p2')
+    def p1(self):
+        print('p1')
+        strings = self.inputReader.readFile('tourist_example_input.txt')
+        print(strings)
 
-def p3():
-    print('p3')
+    def p2(self):
+        print('p2')
+
+    def p3(self):
+        print('p3')
 
 if __name__ == '__main__':
+    sol = Solution()
     prob = sys.argv[1]
     if prob == '1':
-        p1()
+        sol.p1()
     elif prob == '2':
-        p2()
+        sol.p2()
     elif prob == '3':
-        p3()
+        sol.p3()
     else:
         print('Invalid Argument')
