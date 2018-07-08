d4d87b36e8ba94799dc522b4de93b4897246759b
waegaein <waegaein@gmail.com>
2018-07-08 20:10:58 +0900

    CLI

--- QualificationRound/solution.py
+++ QualificationRound/solution.py
@@ -1,4 +1,25 @@
-import InputReader as ir;
+import sys
+import filehandler as fh
 
-temp = ir.InputReader('input')
-temp.readFile('tourist_example_input.txt')
+def p1():
+    print('p1')
+    inputReader = fh.InputReader('input')
+    strings = inputReader.readFile('tourist_example_input.txt')
+    print(strings)
+
+def p2():
+    print('p2')
+
+def p3():
+    print('p3')
+
+if __name__ == '__main__':
+    prob = sys.argv[1]
+    if prob == '1':
+        p1()
+    elif prob == '2':
+        p2()
+    elif prob == '3':
+        p3()
+    else:
+        print('Invalid Argument')
